9a1399881359f06942184457199b023d293e2e73
Kostadin Kostadinov <124677912+Pokepasaj@users.noreply.github.com>
2024-11-10 11:48:06 +0200

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "System flake";
+
+  inputs = {
+    # package repos
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-24_05.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nur.url = "github:nix-community/NUR";
+
+    # home-manager
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # hardware profiles
+    # nixos-hardware.url = "path:///home/micah/.flakes/nixos-hardware";
+    nixos-hardware.url = "github:micahnz/nixos-hardware";
+  };
+
+  outputs = { self, nixpkgs, nur, ... } @ inputs:
+    let
+      nixos-hardware = inputs.nixos-hardware.nixosModules;
+    in
+    {
+      nixosModules = {
+        nixosSystem = { hardware ? "default" }: nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./userpkgs
+            ./system
+            ./system/home-manager
+            nixos-hardware.${hardware}
+            nur.nixosModules.nur
+          ];
+        };
+      };
+    };
+}
